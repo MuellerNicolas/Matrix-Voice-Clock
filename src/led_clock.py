3c8458ef652874f0da0a8be474909a31d5f12f1d
MuellerNicolas <nicolas.moritz.mueller@web.de>
2020-03-28 22:44:26 +0100

    first try

--- src/led_clock.py
+++ src/led_clock.py
@@ -18,7 +18,7 @@ class LEDClock:
             while True:
                 # Adapt led every minute
                 sleep(60)
-                _adapt_led
+                _adapt_led()
         except:
             traceback.print_exc()
         finally:
@@ -27,9 +27,53 @@ class LEDClock:
     def _adapt_led():
         hour = datetime.now().hour
         minute = datetime.now().minute
+        set_hour(hour)
+        set_minute(minute)
         
-    def set_minute():
-        pass
-    
-    def set_hour():
+    def set_hour(hour):
+        if hour > 12:
+            hour = hour % 12
+            switcher = {
+                0: twelve(1, 18),
+                1: odd(2),
+                2: even(3),
+                3: odd(5),
+                4: even(6),
+                5: odd(8),
+                6: even(9),
+                7: odd(11),
+                8: even(12),
+                9: odd(14),
+                10: even(15),
+                11: odd(17),
+                12: twelve()
+            }
+            led.set(switcher.[hour])
+
+        def odd(number):
+            led_array = []
+            for x in range(number):
+                led_array.append("black")
+            led_array.append("blue")
+            while len(led_array) < 18:
+                led_array.append("black")
+        
+        def even(number):
+            led_array = []
+            for x in range(number):
+                led_array.append("black")
+            led_array.append("blue")
+            led_array.append("blue")
+            while len(led_array) < 18:
+                led_array.append("black")
+
+        def twelve():
+            led_array = []
+            led_array.append("blue")
+            for x in range(16):
+                led_array.append("black")
+            led_array.append("blue")
+
+
+    def set_minute(minute):
         pass
