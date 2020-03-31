52ab4ef00bc917504cbe4e2649044d77b4665b6b
MuellerNicolas <nicolas.moritz.mueller@web.de>
2020-03-31 23:25:36 +0200

    ERROR index out of rangegit add .git add .

--- src/led_clock.py
+++ src/led_clock.py
@@ -7,6 +7,9 @@ from math import ceil
 
 class LEDClock:
     def __init__(self):
+        # hour leds
+        self._led_array = []
+        self._hours_set = False
         # led mapping
         self._switcher = {
             0: self._twelve(),
@@ -23,8 +26,6 @@ class LEDClock:
             11: self._odd(17),
             12: self._twelve()
         }
-        # hour leds
-        self._led_array
         # Thread
         self._lock = Lock()
         self._thread_flag = threading.Event()
@@ -45,43 +46,103 @@ class LEDClock:
         hour = datetime.now().hour
         minute = datetime.now().minute
         self._set_hour(hour)
+        self._hours_set = True
         self._set_minute(minute)
-        
+        self._led_array = []
+        self._hours_set = False
+
     def _set_hour(self, hour):
         if hour > 12:
             hour = hour % 12
-
+        print("hour", hour)
+        print("after hour")
+        print(self._switcher)
+        print("")
         led.set(self._switcher[hour])
 
     def _set_minute(self, minute):
         minute = ceil(minute/5)
+        print("minute", minute)
+        print("after hour")
+        print(self._switcher)
+        print("")
         led.set(self._switcher[minute])
 
 
     # Methods setting the leds
     def _odd(self, number):
-        self._led_array = []
-        for x in range(number-1):
-            self._led_array.append("black")
-        self._led_array.append("blue")
+        local_led_array = []
+        for x in range(number-2):
+            if(not self._hours_set and self._led_array[len(local_led_array)-1] != "blue"):
+                self._led_array.append("black")
+                local_led_array.append("black")
+            local_led_array.append("black")
+        if (self._hours_set):
+            if(self._led_array[len(local_led_array)-1] == "blue"):
+                self._led_array.append("yellow")
+                local_led_array.append("yellow")
+            else:
+                self._led_array.append("red")
+                local_led_array.append("red")
+        else:
+            self._led_array.append("blue")
+            local_led_array.append("blue")
         while len(self._led_array) < 18:
-            self._led_array.append("black")
+            if(not self._hours_set and self._led_array[len(local_led_array)-1] != "blue"):
+                self._led_array.append("black")
+                local_led_array.append("black")
+            local_led_array.append("black")
         return self._led_array
     
     def _even(self, number):
-        self._led_array = []
-        for x in range(number-1):
-            self._led_array.append("black")
-        self._led_array.append("blue")
-        self._led_array.append("blue")
+        local_led_array = []
+        for x in range(number-2):
+            if(not self._hours_set and self._led_array[len(local_led_array)-1] != "blue"):
+                self._led_array.append("black")
+                local_led_array.append("black")
+            local_led_array.append("black")
+
+        if (self._hours_set):
+            if(self._led_array[len(local_led_array)-1] == "blue"):
+                self._led_array.append("yellow")
+                self._led_array.append("yellow")
+                local_led_array.append("yellow")
+                local_led_array.append("yellow")
+            else:
+                self._led_array.append("red")
+                self._led_array.append("red")
+                local_led_array.append("red")
+                local_led_array.append("red")
+        else:
+            self._led_array.append("blue")
+            self._led_array.append("blue")
+            local_led_array.append("blue")
+            local_led_array.append("blue")
+
         while len(self._led_array) < 18:
-            self._led_array.append("black")
+            if(not self._hours_set and self._led_array[len(local_led_array)-1] != "blue"):
+                self._led_array.append("black")
+                local_led_array.append("black")
+            local_led_array.append("black")
         return self._led_array
 
     def _twelve(self):
-        self._led_array = []
-        self._led_array.append("blue")
-        for x in range(16-1):
+        local_led_array = []
+        if (self._hours_set):
+            if(self._led_array[0] == "blue"):
+                self._led_array.append("yellow")
+                self._led_array.append("yellow")
+                local_led_array.append("yellow")
+                local_led_array.append("yellow")
+            else:
+                self._led_array.append("red")
+                local_led_array.append("red")
+        else:
+            self._led_array.append("blue")
+            local_led_array.append("blue")
+        for x in range(15):
             self._led_array.append("black")
+            local_led_array.append("black")
         self._led_array.append("blue")
+        local_led_array.append("blue")
         return self._led_array
